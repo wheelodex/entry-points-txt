10c8b7a3716134b3b7a0c6d439047a842ceca5ec
John T. Wodder II <git@varonathe.org>
2020-07-19 22:51:59 +0000

    Packaging

---
+++ src/entry_points_txt.py
@@ -0,0 +1,11 @@
+"""
+Read & write entry_points.txt files
+
+Visit <https://github.com/jwodder/entry-points-txt> for more information.
+"""
+
+__version__      = '0.1.0.dev1'
+__author__       = 'John Thorvald Wodder II'
+__author_email__ = 'entry-points-txt@varonathe.org'
+__license__      = 'MIT'
+__url__          = 'https://github.com/jwodder/entry-points-txt'

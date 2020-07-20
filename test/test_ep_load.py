d565c1c2f366effa937b191e2c5e4e714d790976
John T. Wodder II <git@varonathe.org>
2020-07-20 17:25:45 +0000

    Test EntryPoint.load() method

---
+++ test/test_ep_load.py
@@ -0,0 +1,29 @@
+import configparser
+import email.message
+import pytest
+from   entry_points_txt import EntryPoint
+
+@pytest.mark.parametrize('ep,obj', [
+    (
+        EntryPoint('group', 'foo', 'configparser', None, ()),
+        configparser,
+    ),
+    (
+        EntryPoint('group', 'foo', 'configparser', 'ConfigParser', ()),
+        configparser.ConfigParser,
+    ),
+    (
+        EntryPoint('group', 'foo', 'configparser', 'ConfigParser.BOOLEAN_STATES', ()),
+        configparser.ConfigParser.BOOLEAN_STATES,
+    ),
+    (
+        EntryPoint('group', 'foo', 'email.message', None, ()),
+        email.message,
+    ),
+    (
+        EntryPoint('group', 'foo', 'email.message', 'EmailMessage', ()),
+        email.message.EmailMessage,
+    ),
+])
+def test_ep_load(ep, obj):
+    assert ep.load() is obj

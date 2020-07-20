e2f05ee669e3463cde1a0e0edb32453db2b5247e
John T. Wodder II <git@varonathe.org>
2020-07-20 17:18:53 +0000

    Test to_line() method

---
+++ test/test_to_line.py
@@ -0,0 +1,31 @@
+import pytest
+from   entry_points_txt import EntryPoint
+
+@pytest.mark.parametrize('ep,line', [
+    (
+        EntryPoint('group', 'foo', 'bar', None, ()),
+        'foo = bar',
+    ),
+    (
+        EntryPoint('group', 'foo', 'bar', 'baz', ()),
+        'foo = bar:baz',
+    ),
+    (
+        EntryPoint('group', 'foo', 'bar', None, ('xtra',)),
+        'foo = bar [xtra]',
+    ),
+    (
+        EntryPoint('group', 'foo', 'bar', 'baz', ('xtra',)),
+        'foo = bar:baz [xtra]',
+    ),
+    (
+        EntryPoint('group', 'foo', 'bar', None, ('xtra', 'ytra')),
+        'foo = bar [xtra,ytra]',
+    ),
+    (
+        EntryPoint('group', 'foo', 'bar', 'baz', ('xtra', 'ytra')),
+        'foo = bar:baz [xtra,ytra]',
+    ),
+])
+def test_to_line(ep, line):
+    assert ep.to_line() == line
